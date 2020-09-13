aa0bc215aa69751a45c5e58959d39617d5ae0245
juaneduardoflores <juanedflores@gmail.com>
2020-09-13 12:50:59 -0500

    added and finished 004

---
+++ python/004_reversestring.py
@@ -0,0 +1,22 @@
+# Code Kata #4 - 8 kyu
+# description: Complete the solution so that it
+# reverses the string passed into it.
+# example: 'world'  =>  'dlrow'
+
+# My Solution
+def solution(string):
+    return string[::-1]
+
+
+# Solution with anonymous function
+# and that is a one liner
+def solution1(s): return s[::-1]
+
+
+print(solution("world"))
+
+# What I learned:
+# * The pass keyword, as a placeholder for future code.
+# * How reverse() or [::-1] works on the surface:
+#       for char in range(len(string)-1, -1, -1):
+#           return string[char]
